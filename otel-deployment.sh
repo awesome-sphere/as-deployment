10995ec9052ab2da8e8e27df427c05742e7c2569
suhavni <suhavni@gmail.com>
2022-07-22 00:29:35 +0700

    update seating consumer svc to pull from the correct repo

--- otel-deployment.sh
+++ otel-deployment.sh
@@ -5,14 +5,12 @@ kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/
 
 # function to install opentelemetry-operator
 apply_otel_operator() {
-    sleep 10 # wait until cert-manager is ready
     kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml
 }
 
 # try to get opentelemetry-operator running until connected to cert-manager
 apply_otel_operator
 while [ $? -ne 0 ]; do
+    sleep 10 # wait until cert-manager is ready
     apply_otel_operator
 done
-
-kubectl apply -f ./opentelemetry/
