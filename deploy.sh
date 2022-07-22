2225fae73e4183a01656813f8e8a0097b2d7c885
suhavni <suhavni@gmail.com>
2022-07-22 08:10:05 +0700

    attempted to fix deployment files

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 
 # ./otel-deployment.sh
 ./kafka-deployment.sh
-kubectl apply -R -f .
-# kubectl apply -f ./databases/
-# kubectl apply -f ./services/
+
+kubectl apply -f ./databases
+kubectl apply -f ./services
+
+kubectl delete -f ./services/frontend-svc.yaml
+kubectl apply -f ./services/frontend-svc.yaml
